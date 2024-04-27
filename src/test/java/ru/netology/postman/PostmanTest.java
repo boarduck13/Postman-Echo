721a2ec4975ec8340fcaa1db52ee81f43827bed2
Andrey <justupor@gmail.com>
2024-04-27 19:36:02 +0300

    add fail tests..

--- src/test/java/ru/netology/postman/PostmanTest.java
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hi postman!"))
+                .body("data", equalTo("Hi postman"))
         ;
     }
 }
